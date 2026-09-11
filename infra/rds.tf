4b3a3aa28de4c55e175b7e0fa16e2e1ab6ec2ca9
HenEarl11 <161829337+HenEarl11@users.noreply.github.com>
2026-09-11 16:03:08 +0100

    Update infra/rds.tf
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- infra/rds.tf
+++ infra/rds.tf
@@ -23,7 +23,7 @@ resource "aws_db_instance" "main" {
   vpc_security_group_ids = [aws_security_group.db.id]
 
   publicly_accessible     = true
-  multi_az                = false
+  multi_az = true
   backup_retention_period = 0
   skip_final_snapshot     = true
   deletion_protection     = false
